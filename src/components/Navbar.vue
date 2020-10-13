00680af0e7e2322a0567556a10ffa905639aba15
satoshix13 <satoshix13@gmail.com>
2020-10-13 10:29:40 +0200

    task tracker commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo">Tasks</router-link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <router-link tag="li" to="/" exact active-class="active"><a href="">create</a></router-link>
+        <router-link tag="li" to="/list" active-class="active"><a href="">list</a></router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// import List from '../views/List.vue'
+import Task from '../views/Task.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,13 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/list',
+    name: 'List',
+    component: () => import('../views/List.vue')
+  },
+  { path: '/task/:id',
+    name: 'Task',
+    component: Task
   }
 ]
 
